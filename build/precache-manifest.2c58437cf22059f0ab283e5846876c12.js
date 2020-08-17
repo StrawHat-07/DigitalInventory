ebc4dddc54e07cf896a3250d8624be7d3e1fef2c
Ishan Chawla <ishan.chawla@airtel.com>
2020-08-17 16:01:10 +0530

    Added build files

---
+++ build/precache-manifest.2c58437cf22059f0ab283e5846876c12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7d5e2998c84b5ffe695f0faa7b908b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3574e1ea3b770a32c8c",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "78730a9d3b7a719bce18",
+    "url": "/static/css/main.3c882711.chunk.css"
+  },
+  {
+    "revision": "a3574e1ea3b770a32c8c",
+    "url": "/static/js/2.70c1cdf9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.70c1cdf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78730a9d3b7a719bce18",
+    "url": "/static/js/main.f8a65435.chunk.js"
+  },
+  {
+    "revision": "9e428172f7d93862edff",
+    "url": "/static/js/runtime-main.ae839be9.js"
+  }
+]);
